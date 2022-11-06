7e9aa2c4d5d4d77720ee30dd2140ef336fb9e1a8
Koko <nksonkar34@gmail.com>
2022-11-06 17:21:42 +0530

    Permanent Drawer LEft added!

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import VideoGrid from "./Components/VideoGrid";
 import RightSidebar from "./Components/RightSidebar";
 import LeftSidebar from "./Components/LeftSidebar/LeftSidebar";
+import PermanentDrawerLeft from "./Components/PermanentDrawerLeft";
 
 function App() {
   return (
     <div
     // style={{ display: "flex" }}
     >
-      <LeftSidebar />
+      <PermanentDrawerLeft />
       <VideoGrid />
       {/* <RightSidebar /> */}
     </div>
