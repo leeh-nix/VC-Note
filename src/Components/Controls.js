25920231c0add7edbcfe07b791e24b6b7e266185
rileyyy <vishh0111@gmail.com>
2022-11-06 18:19:05 +0530

    added tool tip in controls

--- src/Components/Controls.js
+++ src/Components/Controls.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
 import IconButton from "@mui/material/IconButton";
-import { ReactComponent as VolumeOn } from "../Assets/volume.svg";
-import { ReactComponent as VolumeOff } from "../Assets/volumeoff.svg";
+import Tooltip from "@mui/material/Tooltip";
+import { ReactComponent as LowerHand } from "../Assets/lowerhand.svg";
+import { ReactComponent as RaiseHand } from "../Assets/raisehand.svg";
 import { ReactComponent as HangUp } from "../Assets/hangup.svg";
 import { ReactComponent as MicOn } from "../Assets/micon.svg";
 import { ReactComponent as MicOff } from "../Assets/micoff.svg";
@@ -12,7 +13,7 @@ import { ReactComponent as ScreenOn } from "../Assets/screenon.svg";
 import { ReactComponent as ScreenOff } from "../Assets/screenoff.svg";
 
 export default function Controls() {
-  const [volume, setVolume] = useState(true);
+  const [hand, setHand] = useState(false);
   const [mic, setMic] = useState(true);
   const [video, setVideo] = useState(true);
   const [screenshare, setScreenshare] = useState(false);
@@ -20,29 +21,40 @@ export default function Controls() {
   return (
     <Grid container spacing={2} sx={{ textAlign: "center", alignItems: "center" }}>
       <Grid item xs>
-        <IconButton onClick={() => setVolume(!volume)} sx={{ backgroundColor: "#2D2D2D", borderRadius: "20px" }}>
-          {volume ? <VolumeOn /> : <VolumeOff />}
-        </IconButton>
+        <Tooltip title={mic ? "Turn off microphone" : "Turn on microphone"}>
+          <IconButton onClick={() => setMic(!mic)} sx={{ backgroundColor: "#2D2D2D", borderRadius: "20px" }}>
+            {mic ? <MicOn /> : <MicOff />}
+          </IconButton>
+        </Tooltip>
       </Grid>
       <Grid item xs>
-        <IconButton onClick={() => setMic(!mic)} sx={{ backgroundColor: "#2D2D2D", borderRadius: "20px" }}>
-          {mic ? <MicOn /> : <MicOff />}
-        </IconButton>
+        <Tooltip title={video ? "Turn on camera" : "Turn off camera"}>
+          <IconButton onClick={() => setVideo(!video)} sx={{ backgroundColor: "#2D2D2D", borderRadius: "20px" }}>
+            {video ? <VideoOn /> : <VideoOff />}
+          </IconButton>
+        </Tooltip>
       </Grid>
       <Grid item xs>
-        <IconButton sx={{ backgroundColor: "#2D2D2D", borderRadius: "20px", width: "132px" }}>
-          <HangUp />
-        </IconButton>
+        <Tooltip title={"Hang Up"}>
+          <IconButton sx={{ backgroundColor: "#2D2D2D", borderRadius: "20px", width: "132px" }}>
+            <HangUp />
+          </IconButton>
+        </Tooltip>
       </Grid>
       <Grid item xs>
-        <IconButton onClick={() => setVideo(!video)} sx={{ backgroundColor: "#2D2D2D", borderRadius: "20px" }}>
-          {video ? <VideoOn /> : <VideoOff />}
-        </IconButton>
+        <Tooltip title={hand ? "Lower Hand" : "Raise Hand"}>
+          <IconButton onClick={() => setHand(!hand)} sx={{ backgroundColor: "#2D2D2D", borderRadius: "20px" }}>
+            {hand ? <RaiseHand /> : <LowerHand />}
+          </IconButton>
+        </Tooltip>
       </Grid>
+
       <Grid item xs>
-        <IconButton onClick={() => setScreenshare(!screenshare)} sx={{ backgroundColor: "#2D2D2D", borderRadius: "20px" }}>
-          {screenshare ? <ScreenOff /> : <ScreenOn />}
-        </IconButton>
+        <Tooltip title={screenshare ? "Stop Sharing" : "Share screen"}>
+          <IconButton onClick={() => setScreenshare(!screenshare)} sx={{ backgroundColor: "#2D2D2D", borderRadius: "20px" }}>
+            {screenshare ? <ScreenOff /> : <ScreenOn />}
+          </IconButton>
+        </Tooltip>
       </Grid>
     </Grid>
   );
