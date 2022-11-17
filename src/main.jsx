ef1e42aa81bd673faf56366ee642b333788e1afd
rileyyy <vishh0111@gmail.com>
2022-11-17 23:24:16 +0530

    added joining page and utils

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import generateMuiTheme from "./mui/theme";
+import { ThemeProvider } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <ThemeProvider theme={generateMuiTheme()}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
